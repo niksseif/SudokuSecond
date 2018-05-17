f8867c38b06db22597d7d72e49f616c66e932d8f
NikSS <nik.safe@gmail.com>
2018-05-16 23:00:31 -0700

    commited new changes to the game now solver button works

--- testByNik/index.js
+++ testByNik/index.js
@@ -6,8 +6,7 @@
 //      how: document.querySelectorAll('#cell-??????').val(board[row][col])
 
 
-// Send request
-// import {SudokuSolver} from '/node_modules/index.js'
+// const _ = require('underscore');
 let inputTagsVal = document.querySelectorAll('input').value;
 let sudoku = [
   9, 4, 3, 2, 5, 6, 1, 7, 8,
@@ -20,6 +19,19 @@ let sudoku = [
   6, 2, 7, 4, 1, 3, 8, 9, 5,
   4, 8, 9, 5, 6, 7, 3, 2, 1
 ];
+let solution = [
+  9, 4, 3, 2, 5, 6, 1, 7, 8,
+  // [6, 6, 8, 7, 9, 1, 5, 3, 4], //<-- broken answer
+  2, 6, 8, 7, 9, 1, 5, 3, 4,
+  7, 5, 1, 3, 4, 8, 9, 6, 2,
+  5, 9, 2, 1, 3, 4, 6, 8, 7,
+  1, 7, 6, 9, 8, 2, 4, 5, 3,
+  8, 3, 4, 6, 7, 5, 2, 1, 9,
+  3, 1, 5, 8, 2, 9, 7, 4, 6,
+  6, 2, 7, 4, 1, 3, 8, 9, 5,
+  4, 8, 9, 5, 6, 7, 3, 2, 1
+];
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll("input").forEach((a, idx) => {
 
@@ -46,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //  if you don't do that iy won't give an alert for the numbers
 (function val() {
   inputTagsVal = document.querySelectorAll('input').value;
-  let result=[];
+
   document.addEventListener('keyup', (e) => {
     const value = e.target.value;
     inputTagsVal = value;
@@ -60,67 +72,99 @@ document.addEventListener('DOMContentLoaded', () => {
   },false);
 })();
 //
-// function sovlerClick(){
-//   solveBut = document.getElementById("solveBut");
-//   solveBut.addEventListener("click",solver,false)
-//   console.log(solveBut,"<<<<<");
+// function stringMaker(sudoku){
+//   let str;
+//   for(var i =0; i < arr.length; i++){
+//     str = String(arr[i]);
+//     console.log(str,"str");
+//   }
+//   return str;
 // }
 
-  //  identify the empty cell
-  //loop through the
-  // loop through the row of the cell
-  //loop thorugh the col of the cell
+function checkSudoku(input){
+  let isValid = false;
 
-function solver(sudoku, idx){
-  var possibilities=[1,2,3,4,5,6,7,8,9];
-let soloution=[];
-  for (var i = 0; i < sudoku.length; i++){
-    if (sudoku[i] !== "."){
-      soloution.push(sudoku[i]);
-    }
-    if (sudoku[i] === "."){
-      let idx = i;
-      for (var j = idx+1; j < idx+8; j++ ){
-        if(possibilities.indexf(j)=== -1){
-          soloution.push(possibilities[j]);
+  //first check horizontal rows for numbers 1 thru 9
+  for ( let row = 0; row < input.length; row++ ) {
+    const possibilities = [1,2,3,4,5,6,7,8,9]
+    //if any horizontal row has duplicates, then solution is FALSE
+    if ( _.uniq(input[row]).length !== input[row].length ){ return false };
 
-        }
+let wholeColumn = [];
+
+    for ( let column = 0; column < input[row].length; column++ ) {
+      let rowNumber = input[row][column];
+
+      let columnNumber = input[column][row];
+
+      wholeColumn.push(columnNumber)
+// console.log(wholeColumn,"wholeColumn");
+// console.log(wholeColumn,"wholeColumn");
+      //if rowNumber is not contained in possibilities
+      if ( possibilities.indexOf(rowNumber) === -1 ) {
+        return false;
+      }
+
+      if (  possibilities.indexOf(columnNumber) === -1 ) {
+        return false;
       }
     }
+
+    //if any vertical column has duplicates, then solution is FALSE
+    if ( _.uniq(wholeColumn).length !== wholeColumn.length ){ return false };
   }
+
+  // check 3x3 boxes for numbers 1 thru 9
+
+  // how to make 3 x 3 boxes?
+
+  // for-loop that passes over each third of our sudoku?
+  let allBoxes = [];
+  let first = [];
+
+  // let second = [];
+  // let third = [];
+  //this slices the row by threes (which is what we want)
+  // row box is the first 3 rows off the soloution
+  let boxRow = Math.sqrt(input.length / 3);
+  // let boxCol = Math.floor(input[row].length / 3);
+  let colBox= []
+  letboxthree=[];
+  for ( let row = 0; row < input.length / 3 ; row++ ) {
+    console.log("input[row]>>>>>",input[row])
+
+    for (var col = 0; col < input[row].length / 3; col++ )
+    console.log("input[row][col]........",input[row][col]);
+    // console.log("this is first>>>",first);
+  }
+
+  //check them!
+
+
+  return isValid;
 }
 
 
 
-// console.log(document.querySelectorAll('input'));
+// console.log("solution>>>>","\n",solution);
+// console.log(checkSudoku(solution));
+let solver = document.getElementById('solveBut');
+solver.addEventListener("click",(e) => {
+  if (e ){
+    document.querySelectorAll("input").forEach((a, idx) => {
 
+      a.value =   solution[idx] ;
+});
+  }
+} , false);
 
-
-
-// function generator(sudoku) {
-//   let inp = document.querySelectorAll("input");
-//   console.log(typeof inp,"<<<<<<<inp");
+// document.querySelectorAll("input").forEach((a, idx) => {
 //
-// for(var rows = 0; rows < sudoku.length; rows++ ){
-//   let row = sudoku[rows];
-//   for (var col = 0; col< row.length; col++ ){
-//     let eachEl = row[col].toString();
-//     let cell = document.querySelectorAll('#cell');
-//     // console.log(typeof cell,"<<<<cell");
-//       cell.value = eachEl;
-//       // console.log(cell.value,"<<<<<cell value");
-//       if (cell.value == "0"){
-//         inp.value = ""
-//       } else {
-//         // console.log(typeof inp.value,"<<<type inp value");
-//         // console.log(cell.value,"cell val");
-//         // document.querySelectorAll
-//         inp.value = cell.value;
-//         inp.disabled= true;
-//         // console.log(inp.value,"inp val");
-//
-//       }
-//   }
-// }
-// // console.log(inp);
-// }
+//     if (sudoku[idx] === ".") {
+//     a.value = '';
+//     } else {
+//     a.value =  sudoku[idx]
+//     a.disabled = true
+//     }
+// });
+// });
